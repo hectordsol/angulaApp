5c7f5d355220b5d669697fe98bf6b7cc78348984
Hector Dario SOL <hectordsol@gmail.com>
2023-01-16 22:33:24 -0300

    aplicando alert a saludo

--- src/app/components/saludo/saludo.component.ts
+++ src/app/components/saludo/saludo.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
   templateUrl: './saludo.component.html',
   styleUrls: ['./saludo.component.scss']
 })
-export class SaludoComponent {
-  name: string = 'Dario';
+export class SaludoComponent implements OnInit {
+  @Input() name: string = 'Cualquiera';
 
+ngOnInit():void{
+  console.log("ngOnInit en Saludo")
+}
+alertSaludo():void{
+  alert(`Hola ${this.name}. Alerta desde click en botón`)
+}
 }
